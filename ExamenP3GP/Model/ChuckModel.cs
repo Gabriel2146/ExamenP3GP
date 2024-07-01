948151069a0149c3fb3ae31aaccba1c9f8257831
laboratorio <laboratorio@UPEAULA-31703>
2024-07-01 08:08:39 -0500

    modelo chuck

--- ExamenP3GP/Model/ChuckModel.cs
+++ ExamenP3GP/Model/ChuckModel.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExamenP3GP.Model
+﻿namespace ExamenP3GP.Models
 {
-    internal class ChuckModel
+    public class JokeModel
     {
-            
+        public string IconUrl { get; set; }
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public string Value { get; set; }
     }
 }
